6f6d9560393dab786d2609c79d841e32e4bc39d3
Vincent Takahashi <takahasv@ens-epua.univ-savoie.fr>
2022-04-05 17:09:42 +0200

    marche pas

--- arret_de_bus.py
+++ arret_de_bus.py
@@ -1,3 +1,4 @@
+from operator import contains
 from horaires import *
 from math import inf
 
@@ -35,69 +36,89 @@ class Arret_de_bus:
 
     def add_horaire_jf(self, horaire_jf):
         self.horaires_jf.append(horaire_jf) 
-    '''
-    def dijkstra(self, liste_arrets, liste_arrets_a_visiter, dest):
-
-        if liste_arrets==liste_arrets_a_visiter:
-            liste_distances=[inf]*len(liste_arrets)
-            liste_arrets_a_visiter.remove(self)
-            liste_distances[liste_distances.index(self)]=0
-        
-        else :
-            for i in self.arrets_voisins :
-                if 
-    '''
-    def init_liste_distances(self, liste_tot):
-        liste_dist=[inf]*len(liste_tot)
-
-        for i in range (len(liste_tot)):
-            if liste_tot[i]==self:
-                liste_dist[i]=0
-        return liste_dist
-
-    
-    def maj_a_visiter(self, liste):
-        liste.remove(self)
-
-    
-
-    def mise_a_jour(self, liste_dist, liste_tot):
-        n=0
-        for i in range(len(liste_tot)):
-            if liste_tot[i]==self:
-                n=i
-
-        for i in range(len(self.arrets_voisins)) :
-            for j in range(len(liste_tot)):
-                if  i==j:
-                    if liste_dist[n]+1<liste_dist[j]:
-                        liste_dist[j]=liste_dist[n]+1
 
     def djikstra(self, dep, dest) :
         noeud_courant=dep
-        a_visiter= self.liste_arrets([])
+        a_visiter=self.liste_arrets([])
         liste_tot=self.liste_arrets([])
-        liste_dist=self.init_liste_distances(liste_tot)
-        dep.maj_a_visiter(a_visiter)
-        dep.init_liste_distances(liste_tot)
+        liste_dist=init_liste_distances(self ,liste_tot)
+        liste_chemin=init_chemin(dep, liste_tot)
+        print(liste_chemin)
 
-        while noeud_courant!=dest :
-            noeud_courant.mise_a_jour(liste_dist, liste_tot)
+        while a_visiter != [] and noeud_courant != dest:
+            maj_a_visiter(noeud_courant, a_visiter)
+            print("noeud courant : ", noeud_courant.nom)
+            mise_a_jour(noeud_courant, liste_dist, liste_tot, liste_chemin, a_visiter)
+            noeud_courant=get_new_noeud_courant(noeud_courant, a_visiter, liste_dist, liste_tot)
+
+
+            print(liste_chemin)
             print(liste_dist)
-            pass
 
+        return liste_dist
 
-        pass 
-
-    def distance_entre_deux_arrets_adjacents_minutes(self, dest):
+'''
+    def distance_entre_deux_arrets_adjacents_minutes(self, dep,  dest):
             dep=changer_string_en_heure(self.horaires[self.arrets_voisins.index(dest)][0])
-            arr=changer_string_en_heure(dest.horaires[dest.arrets_voisins.index(self)][0])
-            
+            dest=changer_string_en_heure(dest.horaires[dest.arrets_voisins.index(self)][0])
+'''
+
+
+def get_new_noeud_courant(noeud_courant, liste_arrets_a_visiter, liste_distances, liste_tot):
+    distance_min=inf
+    indice_min=-1
+    indice=-1
+    for n in noeud_courant.arrets_voisins:
+        if n in liste_arrets_a_visiter :
+            indice=liste_tot.index(n)
+            if distance_min>=liste_distances[indice]:
+                indice_min=indice
+    return liste_tot[indice_min]
+
+
+def init_liste_distances(self, liste_tot):
+    liste_dist=[inf]*len(liste_tot)
+    for i in range (len(liste_tot)):
+        if liste_tot[i]==self:
+            liste_dist[i]=0
+    return liste_dist
+
+def init_chemin(arret, liste_tot):
+    l=[]
+    for i in range(len(liste_tot)) :
+        l.append([])
+    print(len(l))
+    #for j in range(len(liste_tot)):
+     #   l[i][0]=arret
+    return l
+
+
+def maj_a_visiter(a, liste):
+    liste.remove(a)
+      
+def mise_a_jour(arret, liste_dist, liste_tot, liste_chemin, a_visiter):
+    n=0
+    indice1=liste_tot.index(arret)
+    liste_chemin[indice1].append(arret.nom)
+
+    for i in range(len(arret.arrets_voisins)):
+        if liste_tot[i]==arret:
+            n=i
+
+        for j in range(len(liste_tot)):
+            if  arret.arrets_voisins[i]==liste_tot[j]:
+                if liste_dist[n]+1<liste_dist[j]:
+                    liste_dist[j]=liste_dist[n]+1
+                    for i in range(len(arret.arrets_voisins)) :
+                        if arret.arrets_voisins[i] in a_visiter:
+                            indice2=liste_tot.index(arret.arrets_voisins[i])
+                            liste_chemin[indice2] = liste_chemin[indice1] + liste_chemin[indice2]
+        
 
 if __name__=="__main__":
-    #test avec 3 arrets        a1 -> a2 -> a3
+    #test avec 3 arrets        a1 -> a2 -> a3 -> a4
     a1=Arret_de_bus("soleil levant")
-    a2=Arret_de_bus("cimetierre")
+    a2=Arret_de_bus("cimetiere")
     a3=Arret_de_bus("plessis piquet")
     a4=Arret_de_bus("marche")
 
@@ -133,3 +154,5 @@ if __name__=="__main__":
 
 
     a1.djikstra(a1, a2)
+
+    
